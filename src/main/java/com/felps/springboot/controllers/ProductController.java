c4b31c1f9a517d809ffbe40c8b80abdcb3c53f03
Felipe <felipecastro0208@hotmail.com>
2024-08-03 15:12:54 -0300

    Created the first endpoint and record

---
+++ src/main/java/com/felps/springboot/controllers/ProductController.java
@@ -0,0 +1,29 @@
+package com.felps.springboot.controllers;
+
+import com.felps.springboot.dtos.ProductRecordDto;
+import com.felps.springboot.models.ProductModel;
+import com.felps.springboot.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @PostMapping
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
+}
